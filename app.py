32a7d15c6e2477c75daa73a01794351236e03b6b
karthik120505 <125257926+karthik120505@users.noreply.github.com>
2024-08-22 18:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import google.generativeai as genai
+from PIL import Image
+
 st.title("Welcome to gemini")
+
+# Display the uploaded screenshot
+image_path = r"C:\Users\vmd86\OneDrive\Pictures\Screenshots\Screenshot 2024-08-22 180424.png"
+screenshot = Image.open(image_path)
+st.image(screenshot, caption="Screenshot of Karthik's Profile", use_column_width=True)
+
 genai.configure(api_key="AIzaSyBJJj5qsMgeD7DdT6JcCIzMk9lJOPvHo0Q")
-text = st.text_input("Enter") # @param {type: "string"}
+text = st.text_input("Enter")  # @param {type: "string"}
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
